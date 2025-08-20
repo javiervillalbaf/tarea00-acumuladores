e1684b92ade41b363c846c00496958e440fef22d
Javier Villalba <javiervillalba.04@gmail.com>
2025-08-20 02:21:55 -0300

    done

--- src/acumuladores/Acumuladores.java
+++ src/acumuladores/Acumuladores.java
@@ -17,19 +17,56 @@ public class Acumuladores {
 		}
 		return todosMultiplos;
 	}
-
-	/**
-	 * Dada una matriz de enteros y un número, verifica si existe alguna fila 
-	 * donde todos sus elementos sean múltiplos del número recibido por 
-	 * parámetro.
-	 * 
-	 * Si la matriz está vacía o si el número no es positivo, devuelve falso.
-	 * 
-	 * @param mat
-	 * @param num
-	 * @return
-	 */
-	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) { 
+	
+	public static boolean estaContenidoFila(int num, int[] fila) {
+		boolean estaContenido = false;
+		for (int i = 0; i < fila.length; i++) {
+			estaContenido = estaContenido || num == fila[i];
+		}
+		return estaContenido;
+	}
+	
+	public static boolean hayInterseccionEnUnaFila(int[] fila1, int[] fila2) {
+		boolean hayInterseccion = false;
+		for(int i = 0; i < fila1.length ; i++) {
+			hayInterseccion = hayInterseccion || estaContenidoFila(fila1[i], fila2);
+		}
+		return hayInterseccion;
+	}
+	
+	public static int sumaElementosColumna(int[][] mat, int nColum) {
+		int sumaColumna = 0;
+		for (int i = 0; i < mat.length; i++) {
+			sumaColumna += mat[i][nColum];
+		}
+		return sumaColumna;
+	}
+	
+	public static int sumaElementosFila(int[] fila) {
+		int sumaFila = 0;
+		for (int i = 0; i < fila.length; i++) {
+			sumaFila += fila[i];
+		}
+		return sumaFila;
+	}
+	
+	public static boolean estaContenidoColumna(int num, int[][] mat, int nColum) {
+		boolean estaContenido = false;
+		for (int i = 0; i < mat.length; i++) {
+			estaContenido = estaContenido || num == mat[i][nColum];
+		}
+		return estaContenido;
+	}
+	
+	public static boolean hayInterseccionEnUnaColumna(int[][] mat1, int[][] mat2, int nColum) {
+		boolean hayInterseccion = false; 
+		for (int i = 0; i < mat1.length; i++) {
+			hayInterseccion = hayInterseccion || estaContenidoColumna( mat1[i][nColum], mat2, nColum);
+		}
+		return hayInterseccion;
+	}
+	
+ 	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) { 
 		if (estaVacia(mat) || num <= 0) {
 			return false;
 		}
@@ -40,49 +77,37 @@ public class Acumuladores {
 		return todosMultiplosAlgunaFila;
 	}
 	
-	/**
-	 * Dado 2 matrices se verifica si hay intersección entre las filas de cada
-	 * matriz, fila a fila.
-	 * 
-	 * Si las matrices tienen distinta cantidad de filas o si alguna matriz 
-	 * está vacía, devuelve falso.
-	 * 
-	 * @param mat1
-	 * @param mat2
-	 * @return
-	 */
-	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) {
+		if (estaVacia(mat1) || estaVacia(mat2) || mat1.length != mat2.length) {
+			return false;
+		}
+		boolean hayInterseccionPorFila = true;
+		for (int i = 0; i < mat1.length; i++) {
+			hayInterseccionPorFila = hayInterseccionPorFila && hayInterseccionEnUnaFila(mat1[i], mat2[i]);
+		}
+		return hayInterseccionPorFila;
 	}
 	
-	/**
-	 * Dada una matriz y el índice de una columna, se verifica si existe alguna
-	 * fila cuya suma de todos sus elementos sea mayor estricto que la suma de
-	 * todos los elementos de la columna indicada por parámetro.
-	 * 
-	 * Si el índice de la columna es inválido o la matriz está vacía, devuelve 
-	 * falso.
-	 * 
-	 * @param mat
-	 * @param nColum
-	 * @return
-	 */
-	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) {
+		if (estaVacia(mat) || nColum > mat.length || nColum < 0 ) {
+			return false;
+		}
+		boolean algunaFilaMayorQueColumna = false;
+		for (int i = 0; i < mat.length; i++) {
+			algunaFilaMayorQueColumna = algunaFilaMayorQueColumna || sumaElementosFila(mat[i]) > sumaElementosColumna(mat, nColum);
+		}
+		return algunaFilaMayorQueColumna;
 	}
 	
-	/**
-	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de
-	 * cada matriz, columna a columna.
-	 * 
-	 * Si las matrices tienen distinta cantidad de columnas o alguna matriz 
-	 * está vacía, devuelve falso. 
-	 * 
-	 * @param mat1
-	 * @param mat2
-	 * @return
-	 */
-	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) {
+		if (estaVacia(mat1) || estaVacia(mat2) || mat1[0].length != mat2[0].length) {
+			return false;
+		}
+		boolean hayInterseccionPorColumna = true;
+		for (int i = 0; i < mat1[0].length; i++) {
+			hayInterseccionPorColumna = hayInterseccionPorColumna && hayInterseccionEnUnaColumna(mat1, mat2, i);
+		}
+		
+		return hayInterseccionPorColumna;
 	}
 }
